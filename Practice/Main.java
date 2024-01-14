5c771592a124ffa2627ce86f506c4c25bc60fa89
Abhibhav <abhibhar@gmail.com>
2024-01-14 15:26:27 +0530

    Some changes to make it better

--- Practice/Main.java
+++ Practice/Main.java
@@ -7,9 +7,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int go = 0;
         while(go == 0) {
+            System.out.println("Enter first Number: ");
             int a = sc.nextInt();
+            System.out.println("Enter second Number: ");
             int b = sc.nextInt();
+            System.out.println("Which operation would you like to perform");
             int operator = sc.nextInt();
+            long result = 1;
 
             switch (operator) {
                 case 1:
@@ -35,9 +39,19 @@ public class Main {
                         System.out.println(a % b);
                     }
                     break;
+                case 6:
+                    int base = sc.nextInt();
+                    int exponent = sc.nextInt();
+                    while (exponent != 0) {
+                        result *= base;
+                        --exponent;
+                    }
+                    System.out.println("Answer = " + result);
+                    break;
                 default:
                     System.out.println("Invalid Operator");
             }
+            System.out.println("Do you want to continue");
             go = sc.nextInt();
         }
             System.out.println("Thank you for using simple calculator");
